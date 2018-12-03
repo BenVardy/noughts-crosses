01cabfe5235ae29ebb3f45b23106c64857ae2112
Ben Vardy <ben@vardyfamily.co.uk>
2018-12-03 08:42:14 +0000

    1st version of working app

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(3000, () => console.log('listening on port 3000'));
+
+app.use('/', express.static(`${__dirname}/../build`))
+
+let connections = [null, null];
+
+io.on('connection', socket => {
+    
+    let playerIndex = -1;
+    for (let i in connections) {
+        if (connections[i] === null) {
+            playerIndex = i;
+            break;
+        }
+    }
+
+    socket.emit('player-number', playerIndex);
+
+    if (playerIndex == -1) return;
+
+    connections[playerIndex] = socket;
+
+    socket.broadcast.emit('player-connect', playerIndex);
+
+    socket.on('turn-done', squares => {
+        socket.broadcast.emit('next-turn', squares);
+    })
+
+    socket.on('disconnect', () => connections[playerIndex] = null)
+})
--- src/index.js
+++ src/index.js
@@ -1,12 +1,160 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            myValue: null,
+            xIsNext: 0,
+
+            waitingForPlayer2: true
+        }
+    }
+
+    componentDidMount() {
+        const socket = io();
+
+        socket.on('player-number', playerNumber => {
+            this.setState({ myValue: playerNumber });
+
+            console.log(playerNumber)
+            if (playerNumber == 0) {
+                console.log('waiting');
+                socket.on('player-connect', () => {
+                    console.log('starting');
+                    this.setState({
+                        waitingForPlayer2: false
+                    })
+                })
+            } else {
+                console.log('starting')
+                this.setState({ 
+                    waitingForPlayer2: false
+                });
+            }
+        });
+
+        socket.on('next-turn', squares => {
+            this.setState({
+                squares: squares,
+                xIsNext: !this.state.xIsNext
+            });
+        });
+
+        this.setState({
+            socket
+        })
+
+    }
+
+    handleClick(i) {
+        const { socket } = this.state;
+
+        const squares = this.state.squares.slice();
+        if (squares[i] === null && this.state.myValue == this.state.xIsNext) {
+            squares[i] = this.state.xIsNext ? 'X' : 'O';
+            this.setState({
+                squares,
+                xIsNext: !this.state.xIsNext
+            });
+
+            socket.emit('turn-done', squares)
+        }
+    }
+
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+    
+        return null;
+    }
+
+    render() {
+        const winner = this.calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+        if (!this.state.waitingForPlayer2) {
+            return (
+                <div>
+                    <div className="status">{status}</div>
+                    <div className="board-row">
+                        {this.renderSquare(0)}
+                        {this.renderSquare(1)}
+                        {this.renderSquare(2)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare(3)}
+                        {this.renderSquare(4)}
+                        {this.renderSquare(5)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare(6)}
+                        {this.renderSquare(7)}
+                        {this.renderSquare(8)}
+                    </div>
+                </div>
+            )
+        } else {
+            return <div>Waiting For Player 2...</div>
+        }
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
